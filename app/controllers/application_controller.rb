b03981137b4aa3d6204f82bb3bd7fbced32c2517
ah-james <11ahj11@gmail.com>
2021-01-19 17:06:48 -0500

    Review Show and Create Pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+
+    helpers do
+        def current_user
+            @current_user ||= User.find(session[:user_id]) if session[:user_id]
+        end
+    end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,5 +7,21 @@ class ReviewsController < ApplicationController
 
     def create
         @review = Review.new(review_params)
+        @review.user_id = session[:user_id]
+        if @review.save
+            redirect_to review_path(@review)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @review = Review.find_by_id(params[:id])
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:review_text, :score, :album_id)
     end
 end
