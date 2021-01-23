625b7f33245d11bfee90e0ed8e3f0520773cc960
ah-james <11ahj11@gmail.com>
2021-01-23 15:54:55 -0500

    Showing Error Messages In Views

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :name, presence: true
-    validates :name, length: { minimum: 3 }
+    validates :name, format: { without: /[0-9]/ }
     validates :email, uniqueness: true
     validates :password, length: { in: 5..10}
 end
