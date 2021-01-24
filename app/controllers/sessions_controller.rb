e4b1b824cb15506238694d630aebf2db5be13be5
ah-james <11ahj11@gmail.com>
2021-01-24 01:32:46 -0500

    Starting Omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,10 +13,24 @@ class SessionsController < ApplicationController
         else
             redirect_to login_path
         end
+        # @user = User.find_or_create_by(uid: auth['uid']) do |u|
+        #     u.name = auth['info']['name']
+        #     u.email = auth['info']['email']
+        # end
+      
+        # session[:user_id] = @user.id
+
+        # redirect_to login_path
     end
 
     def destroy
         session.delete :user_id
         redirect_to '/'
+    end
+
+    private
+
+    def auth
+        request.env['omniauth.auth']
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_011116) do
+ActiveRecord::Schema.define(version: 2021_01_24_062626) do
 
   create_table "albums", force: :cascade do |t|
     t.string "title"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2021_01_24_011116) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
   end
 
 end
