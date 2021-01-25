d3da4671859a58187afb7cafe7fc8a42f6b3d8cb
ah-james <11ahj11@gmail.com>
2021-01-24 22:20:16 -0500

    CSS and HTML Changes

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,9 +2,9 @@ module UsersHelper
 
     def user_show_header
         if session[:user_id] == @user.id
-            content_tag(:h1, "Hello #{@user.name}, here are your albums:")
+            content_tag(:h1, "Hello #{@user.name}, here are your favorite albums:")
         else
-            content_tag(:h1, "#{@user.name}'s Albums")
+            content_tag(:h1, "#{@user.name}'s  Favorite Albums")
         end
     end
 
