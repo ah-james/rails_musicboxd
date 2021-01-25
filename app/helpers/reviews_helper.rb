ab72643173daeb304e53d74c0538bccdf3b3c512
ah-james <11ahj11@gmail.com>
2021-01-24 19:40:30 -0500

    Adding Helper Methods to Reviews and Users Views

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,6 +1,26 @@
 module ReviewsHelper
 
-    def highest_score
-        order(:score)
+    def review_index_header
+        if params[:album_id]
+            content_tag(:h1, "All Reviews for #{@album.title} by #{@album.artist.artist_name}")
+        else
+            content_tag(:h1, "All Reviews")
+        end
+    end
+
+    def review_new_header
+        if params[:album_id]
+            content_tag(:h1, "New Album Review for #{@album.title}")
+        else
+            content_tag(:h1, "New Album Review")
+        end
+    end
+
+    def review_show_header
+        if @review.user_id == session[:user_id]
+            content_tag(:h1, "Your Review for #{@review.album.title} by #{@review.album.artist.artist_name}")
+        else
+            content_tag(:h1, "#{@review.user.name}'s Review for #{@review.album.title} by #{@review.album.artist.artist_name}")
+        end
     end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,18 @@
 module UsersHelper
+
+    def user_show_header
+        if session[:user_id] == @user.id
+            content_tag(:h1, "Hello #{@user.name}, here are your albums:")
+        else
+            content_tag(:h1, "#{@user.name}'s Albums")
+        end
+    end
+
+    def user_show_songs
+        if session[:user_id] == @user.id
+            content_tag(:h3, "Your Favorite Songs:")
+        else
+            content_tag(:h3, "#{@user.name}'s Favorite Songs:")
+        end
+    end
 end
